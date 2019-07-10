6525cb04fe9fca1fd486f6d8f53b566445eae593
Todd Goates <toddgoates@gmail.com>
2019-07-10 11:49:52 -0600

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,60 @@
+const app = new Vue({
+  el: "#app",
+
+  data: {
+    tasks: [],
+    newTask: ""
+  },
+
+  computed: {
+    totalHours() {
+      let totalHours = 0;
+      this.tasks.forEach(t => {
+        totalHours += parseFloat(t.timeSpent);
+      });
+      return totalHours.toFixed(2);
+    }
+  },
+
+  created() {
+    this.init();
+  },
+
+  updated() {
+    this.updateLocalStorage();
+  },
+
+  methods: {
+    init() {
+      if (localStorage.getItem("myTasks") !== null) {
+        this.tasks = JSON.parse(localStorage.getItem("myTasks"));
+      }
+    },
+
+    saveTask() {
+      let taskToSave = {};
+
+      taskToSave.id = Date.now();
+      taskToSave.task = this.newTask;
+      taskToSave.completed = false;
+      taskToSave.timeSpent = 0;
+
+      this.tasks.push(taskToSave);
+
+      this.newTask = "";
+      this.$refs["task-input"].focus();
+    },
+
+    updateLocalStorage() {
+      localStorage.setItem("myTasks", JSON.stringify(this.tasks));
+    },
+
+    removeTask(task) {
+      this.tasks.splice(this.tasks.indexOf(task), 1);
+    },
+
+    completeTask(task) {
+      task.completed = !task.completed;
+    }
+  }
+});
